4906a7256d679d0e59c81090f086a20ee99f8c98
sai santhoshi <saisanthoshi.17.it@anits.edu.in>
2021-09-20 13:18:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route exact path={"/dashboard"}> 
+        <Dashboard/>
+        </Route>
+        </Switch>
+      </Router>
+      
+       
+        
+       
+      </div>
+    
+    )
+  }
 }
-
 export default App;
